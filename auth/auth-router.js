0abc6408c445e05e70bbfd8acaab9904825139a5
igor <embiggenerd@gmail.com>
2020-02-28 12:06:23 -0500

    register route and auth helpers

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,15 +1,28 @@
 const router = require('express').Router();
+const {
+  hashPassword,
+  comparePasswords,
+  generateToken
+} = require('./auth-helpers')
 
-router.post('/register', (req, res) => {
+const db = require('../database/dbConfig')
+
+router.post('/register', async (req, res) => {
   try {
-  const { username, password } = req.body
-  if(!username || !passowrd) {
-    return res.status(400).json({message: "Username and Password are required!"})
-  }
+    const { username, password } = req.body
+    if (!username || !password) {
+      return res.status(400).json({ message: "Username and Password are required!" })
+    }
 
-  
+    const hashedPass = hashPassword(password)
 
-  }catch(e){
+    const [id] = await db('users').insert({ username, password:hashedPass })
+
+    const [newUser] = await db('users').select('username', 'id').where({ id })
+
+    res.json(newUser)
+
+  } catch (e) {
     console.log(e)
   }
   // implement registration
